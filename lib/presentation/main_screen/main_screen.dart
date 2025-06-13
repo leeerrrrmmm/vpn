0849a9fc69560b47da30efa2956b800aea79ff88
Lerm <mychko.mychko@gmail.com>
2025-06-13 18:22:03 +0300

    minor change of MainScreen

--- lib/presentation/main_screen/main_screen.dart
+++ lib/presentation/main_screen/main_screen.dart
@@ -5,6 +5,16 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Main Screen')));
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {
+            //TODO make drawable
+          }, icon: Icon(Icons.menu))
+        ],
+        title: Text('HIDE VPN'),
+        centerTitle: true,
+      ),
+    );
   }
 }
